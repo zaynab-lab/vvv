2adcba2b6abb74532cbc4b32df1afe64d1947ceb
Zaynab <75333513+zaynab-lab@users.noreply.github.com>
2020-12-08 17:49:56 +0200

    dd

--- public/js/styles.js
+++ public/js/styles.js
@@ -1,11 +1,12 @@
 export const styles = {
   primaryColor: "#ff9900",
   primaryColorLight: "sandybrown",
-  secondaryColor: "#60b0f4",
-  thirdColor: "#fffaf0"
-  // primaryColor: "#ff8000",
-  // primaryColorLight: "#ff8000",
-  // secondaryColor: "#007fff"
+  secondaryColor: "#60b0f4"
+  // thirdColor: "#fffaf0",
+  // thirdColor: "#fffaf8",
+  // primaryColor: "#d22d50",
+  // primaryColorLight: "#d22d50",
+  // secondaryColor: "#2DD2AF"
 };
 
 // #00aadd
